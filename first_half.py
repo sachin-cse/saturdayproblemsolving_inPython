1b0a21309c1f721cec83bb288834eddbc6486300
sachin <mandalsachin725@gmail.com>
2024-03-10 11:18:08 +0530

    solved

---
+++ first_half.py
@@ -0,0 +1,11 @@
+
+# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+# first_half('WooHoo') → 'Woo'
+# first_half('HelloThere') → 'Hello'
+# first_half('abcdef') → 'abc'
+
+def first_half(str):
+  if len(str)%2 == 0:
+    str_len = len(str)//2
+    return str[:str_len]
